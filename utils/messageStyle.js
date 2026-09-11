7f3bfbf5bcead3e87c55b3f8ed2dc12a5fa8b250
zonetechniquemedia-hash <zonetechnique.media@gmail.com>
2026-09-11 20:05:38 +0100

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "Géto bot",
+        body: "christ adams",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
