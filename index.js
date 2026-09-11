3243c37cf52da8dac70e454cb579e2a8cdbf5634
zonetechniquemedia-hash <zonetechnique.media@gmail.com>
2026-09-11 19:43:53 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './Digix/crew.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
