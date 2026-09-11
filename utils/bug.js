ed8ebe200cbcf7f84a90a16772c1801ed05e7f78
zonetechniquemedia-hash <zonetechnique.media@gmail.com>
2026-09-11 20:19:37 +0100

    Create bug.js

---
+++ utils/bug.js
@@ -0,0 +1,24 @@
+async function bug(message, client, texts, num) {
+    try {
+        const remoteJid = message.key?.remoteJid;
+        await client.sendMessage(remoteJid, {
+            image: { url: `database/${num}.jpg` },
+            caption: `> ${texts}`,
+            contextInfo: {
+                externalAdReply: {
+                    title: "Join Our WhatsApp Channel",
+                    body: " 𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪 ",
+                    mediaType: 1,
+                    thumbnailUrl: `https://whatsapp.com/channel/0029VbBT7FdLCoX1TDyQQb1B`,
+                    renderLargerThumbnail: false,
+                    mediaUrl: `${num}.jpg`,
+                    sourceUrl: `${num}.jpg`
+                }
+            }
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default bug;
