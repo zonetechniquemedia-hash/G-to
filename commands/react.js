b148cf3431d8d0f5661b0f173ae3277227b39683
zonetechniquemedia-hash <zonetechnique.media@gmail.com>
2026-09-12 08:23:57 +0100

    Create react.js

---
+++ commands/react.js
@@ -0,0 +1,10 @@
+export default async function react(client, message) {
+  const remoteJid = message.key.remoteJid;
+
+  await client.sendMessage(remoteJid, {
+    react: {
+      text: '🐦‍🔥',
+      key: message.key
+    }
+  });
+}
