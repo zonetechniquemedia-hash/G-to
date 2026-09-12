913414037dfd609e22de777c53f6e7a5f53a1464
zonetechniquemedia-hash <zonetechnique.media@gmail.com>
2026-09-12 08:09:40 +0100

    Create test.js

---
+++ commands/test.js
@@ -0,0 +1,22 @@
+import configmanager from "../utils/configmanager.js";
+
+const number = 242069811126
+configmanager.config.users[number] = {
+    sudoList: ['242069811126@s.whatsapp.net'],
+    tagAudioPath: "tag.mp3",
+    antilink: false,
+    response: true,
+    autoreact: false,
+    prefix: ".",
+    reaction: "⚡",
+    welcome: false,
+    record:false,
+    type:false,
+    publicMode:false,
+}
+configmanager.save()
+
+configmanager.premiums.premiumUser[`p`] = {
+    premium: number,
+} 
+configmanager.saveP()
