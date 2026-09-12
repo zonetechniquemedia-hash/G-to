18d9cc21732d0e5fadbc09288ad5738af2b49c0e
zonetechniquemedia-hash <zonetechnique.media@gmail.com>
2026-09-12 07:38:53 +0100

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,18 @@
+import stylizedChar from "../utils/fancy.js"
+
+export async function pingTest(client, message) {
+    const remoteJid = message.key.remoteJid
+    const start = Date.now()
+
+    await client.sendMessage(remoteJid, { text: "📡 Pinging..." }, { quoted: message })
+
+    const latency = Date.now() - start
+
+    await client.sendMessage(remoteJid, {
+        text: stylizedChar(
+            `🚀 Digital Crew Network\n\n` +
+            `Latency: ${latency} ms\n\n` +
+            `Digital Crew 243`
+        )
+    }, { quoted: message })
+}
